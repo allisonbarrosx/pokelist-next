e59dfed3219ec30ee94a1fdebe0c4d8befd42781
Allison Barros <bx-guy@bx-guy>
2025-08-08 13:29:48 -0400

    Fixed layout issue with width

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -37,7 +37,7 @@ export default function RootLayout({
       <LoadingBar />
       <html lang="en">
         <body
-          className={`${geistSans.variable} ${geistMono.variable} antialiased w-screen h-screen`}
+          className={`${geistSans.variable} ${geistMono.variable} antialiased h-screen`}
         >
           {children}
         </body>
